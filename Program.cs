1a8ddc8ab4d877b12cd33c9ac23fcf2c6e3dfa8e
Anton <antonukin@icloud.com>
2023-03-09 19:30:43 +0300

    code added

--- Program.cs
+++ Program.cs
@@ -2,5 +2,34 @@
 меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
 выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
 исключительно массивами.
+Примеры:
+["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+["1234", "1567", "-2", "computer science"] -> ["-2"]
+["Russia", "Denmark", "Kazan"] -> []
 */
 
+string[] GetString()
+{
+    string[] words = new string[6];
+    for (int i = 0; i < words.Length; i++)
+    {
+        Console.WriteLine("Введите слово");
+        words[i] = Console.ReadLine();
+    }
+return words;
+}
+
+void PrintArray(string[] statement)
+{
+    for (int i = 0; i < statement.Length; i++)
+    {
+        if(statement[i].Length < 4)
+        {
+            Console.Write($"{statement[i]} ");
+        }
+        else continue;
+    }
+}
+
+string[] newWords = GetString();
+PrintArray(newWords);
